021ebee8a14f9f86d84c51044491f536e7548722
tony-luisi <t.luisi@gmail.com>
2016-04-03 12:42:49 +1200

    initial build

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+    /*"start": "webpack-dev-server --progress --colors --inline-sourcemap --hot --watch-poll --content-base public",*/
